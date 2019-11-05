f3d0ba935f1d1ae7231e3e11843de29fd6a5b5b2
Brian Nicholls <nichol88@gmail.com>
2019-11-05 14:56:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # My Code here....
+def map_to_negativize(source_array)
+end
+
+def map_to_no_change(source_array)
+end
+
+def map_to_double(source_array)
+end
+
+def map_to_square(source_array)
+end
