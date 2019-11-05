c46a6066f61a39add60380b953dee19a21778b6e
Brian Nicholls <nichol88@gmail.com>
2019-11-05 14:59:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,6 +16,12 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  count = 0
+  result = []
+  while count < source_array.length do
+    result.push(source_array[count] * 2)
+    count += 1
+  end
 end
 
 def map_to_square(source_array)
